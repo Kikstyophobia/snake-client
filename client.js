faf49ebc541cf2549d34075b672e992247fca1d4
Calvin Chung <calvin-chung@live.com>
2022-03-16 19:50:23 -0700

    snake game works

--- client.js
+++ client.js
@@ -1,2 +1,24 @@
-const connect = require('./play');
+const net = require('net');
 
+const connect = function () {
+  const conn = net.createConnection({
+    host: "165.227.47.243",
+    port: 50541,
+  });
+
+  conn.on("data", (data) => {
+    console.log(data);
+  });
+
+  conn.setEncoding("utf8");
+
+  conn.on("connect", () => {
+    console.log('Established connection!')
+    console.log("Successfully connected to game server.")
+    conn.write("Name: CHC");
+  });
+  
+  return conn;
+};
+
+module.exports = {connect};
--- play.js
+++ play.js
@@ -1,63 +1,6 @@
-const net = require("net");
-
-const setupInput = function () {
-  const stdin = process.stdin;
-  stdin.setRawMode(true);
-  stdin.setEncoding("utf8");
-  stdin.resume();
-  stdin.on("data", handleUserInput);
-  return stdin;
-};
-
-// exits with Ctrl + C 
-const handleUserInput = function () {
-  process.stdin.on('data', (key) => {
-    if (key === '\u0003') {
-    process.exit();
-    }
-  })
-};
-
-// establishes a connection with the game server
-const connect = function () {
-  const conn = net.createConnection({
-    host: "165.227.47.243",
-    port: 50541,
-  });
-
-  conn.on("connect", () => {
-    console.log('Established connection!')
-  });
-
-  conn.on("data", (data) => {
-    console.log(data);
-  });
-
-  conn.on("connect", () => {
-    console.log("Successfully connected to game server.")
-  });
-
-  conn.on("connect", () => {
-    conn.write("Name: CHC");
-  });
-
-  conn.on("connect", () => {
-    conn.write("Move: up");
-  });
-
-  conn.on("connect", () => {
-    setTimeout(() => {
-    conn.write("Move: left")
-    }, 1000);
-  });
-
-  // interpret incoming data as text
-  conn.setEncoding("utf8");
-
-  return conn;
-};
+const { connect } = require("./client");
+const { setupInput } = require("./input");
 
 console.log("Connecting ...");
-connect();
 
-module.exports = connect;
+setupInput(connect());
